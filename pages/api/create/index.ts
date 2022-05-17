6e8d8ac21c0b98f21c472a4eb3435c57164f93ee
instrovate <lms@instrovate.com>
2022-05-17 10:53:57 +0530

    final

--- pages/api/create/index.ts
+++ pages/api/create/index.ts
@@ -1,5 +1,6 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
+import { NextApiRequest, NextApiResponse } from 'next';
+
 
 
 export default async function create(req, res) {
